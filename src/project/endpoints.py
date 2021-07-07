8d093a6e71801037b9a155f9d7fb0b2823d0b6fa
crispoh <calvear2014@gmail.com>
2021-07-07 01:09:10 -0400

    funciona el jwt, se debe corregir

--- src/project/endpoints.py
+++ src/project/endpoints.py
@@ -1,4 +1,7 @@
+import jwt
+from datetime import datetime, timedelta
 from flask import Blueprint, request, jsonify
+from werkzeug.exceptions import NotFound, Unauthorized
 from .models import Medico
 from . import db 
 
@@ -8,6 +11,10 @@ medicos_blueprint = Blueprint('medicos', __name__)
 ############### ingresar medico #######################
 @medicos_blueprint.route('/registro', methods=['POST'])
 def index():
+  ####Autorizacion####
+  if not check_token():
+        raise Unauthorized
+
   datos = request.get_json()
 
   medico = Medico(**datos)
@@ -21,6 +28,10 @@ def index():
 @medicos_blueprint.route('/medicos', methods=['GET'])
 def listar_medicos():
 
+    ####Autorizacion####
+    if not check_token():
+        raise Unauthorized
+
     medicos = Medico.query.all()
 
     lista_medicos = []
@@ -33,6 +44,10 @@ def listar_medicos():
 ################## mostrar por id ######################
 @medicos_blueprint.route('/medicos/<id>', methods=['GET'])
 def obtener_medico(id):
+    ####Autorizacion####
+    if not check_token():
+        raise Unauthorized
+
     medico = Medico.query.get_or_404(id)
     #medico = Medico.query.filter_by(id=id).first()
 
@@ -41,9 +56,14 @@ def obtener_medico(id):
 
     return medico_a_dict(medico), 200
 
-################## modificar id ########################
+################## modificar datos de id ########################
 @medicos_blueprint.route('/medicos/<id>', methods=['PUT'])
 def actualizar_medico(id):
+
+    ####Autorizacion####
+    if not check_token():
+        raise Unauthorized
+
     medico = Medico.query.get_or_404(id)
     #medico = Medico.query.filter_by(id=id).first()
     datos = request.get_json()
@@ -56,9 +76,14 @@ def actualizar_medico(id):
 
     return medico_a_dict(medico), 200
 
-################# eliminar id #########################
+################# eliminar datos de id #########################
 @medicos_blueprint.route('/medicos/<id>', methods=['DELETE'])
 def eliminar_medico(id):
+
+    ####Autorizacion####
+    if not check_token():
+        raise Unauthorized
+
     medico = Medico.query.get_or_404(id)
     #medico = Medico.query.filter_by(id=id).first()
 
@@ -66,10 +91,62 @@ def eliminar_medico(id):
     db.session.commit()
 
     return '', 204
+################ login con jwt ##################################
+@medicos_blueprint.route('/login', methods=['GET', 'POST'])
+def login():
+    datos = request.get_json()
+    
+    medico  = Medico.query.filter_by(
+        email=datos['email'],password=datos['password']).first()
+
+    if medico is None: 
+        raise NotFound
+
+    ##Secret Code##
+    secret = 'GtP4CVkAxMx2KjKy'
+
+    payload = {
+        'sub' : medico.id,
+        'iat' : datetime.utcnow(),
+        'exp' : datetime.utcnow() + timedelta(days=1)
+    }
+
+    return jwt.encode(payload, secret, algorithm='HS256')
 
 ################ datos ################################
 def medico_a_dict(medico):
   return {
         'id': medico.id,
-        'email': medico.email
+        'email': medico.email,
+        'nombre' : medico.nombre,
+        'password' : medico.password
   }
+##############def autorizacion##########################
+
+def check_token():
+    auth_header = request.headers.get('Authorization')
+
+    if not auth_header:
+        return False
+
+    ### divide el entre BEARER Y EL CODE ###
+    partes = auth_header.split(' ')
+    ### verifica
+    if len(partes) != 2:
+        return False
+
+    token = partes[1]
+    ##Secret Code##
+    secret= 'GtP4CVkAxMx2KjKy'
+
+    try:
+      payload = jwt.decode(token, secret, algorithms=['HS256'])
+
+      exp = payload['exp']
+
+      if datetime.utcnow() > datetime.fromtimestamp(exp):
+          return False
+      
+      return True
+    except:
+      return False
--- src/project/models.py
+++ src/project/models.py
@@ -1,10 +1,18 @@
 from . import db
 
+            ####### Modelo Base de datos ########
+### al agregar dato se debe: docker-compose exec flask_app flask db init 
+### docker-compose exec flask_app flask db migrate
+### docker-compose exec flask_app flask db upgrade
+
 class Medico (db.Model):
     id = db.Column (db.Integer, primary_key=True, autoincrement=True)
+    nombre = db.Column (db.String, nullable=False)
     email = db.Column (db.String, nullable=False)
     password = db.Column (db.String, nullable=False)
-    
+
+             ### ignorar###
+
   #  def __init__(self, email, password):
   #   self.email = email
   #   self.password = password
